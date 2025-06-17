c4e9f769d7325e0a6119fd03a0902de9f17cdf6e
mySahib <bshsahibhero@gmail.com>
2025-06-17 23:00:59 +0530

    Integrate Cloudinary for persistent image storage

--- enquiry_saas_project/settings.py
+++ enquiry_saas_project/settings.py
@@ -88,9 +88,9 @@ DATABASES = {
 }
 
 CLOUDINARY = {
-    "cloud_name": os.environ.get("CLOUDINARY_CLOUD_NAME", "YOUR_CLOUD_NAME"),
-    "api_key": os.environ.get("CLOUDINARY_API_KEY", "YOUR_API_KEY"),
-    "api_secret": os.environ.get("CLOUDINARY_API_SECRET", "YOUR_API_SECRET"),
+    "cloud_name": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "api_key": os.environ.get("CLOUDINARY_API_KEY"),
+    "api_secret": os.environ.get("CLOUDINARY_API_SECRET"),
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
